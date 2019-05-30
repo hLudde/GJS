6e2026d431ce5622b4b03c7e02daba83554235a3
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-05-30 20:45:55 +0200

    added some memory functionality and memory tests

---
+++ src/memory.js
@@ -0,0 +1,36 @@
+var memory = new Array(parseInt(process.env.MEMORY_SIZE)+1);
+exports.load = (pointer) => {
+    if(validatePointer(pointer)){
+        return memory[pointer]
+    }
+}
+
+exports.set = (pointer, value)=>{
+    if(validatePointer(pointer)&&validateValue(value)){
+        memory[pointer] = value;
+    }
+}
+
+exports.init = () =>{
+    for(var i = 0; i<memory.length; i++){
+        memory[i]=0x0000;
+    }
+}
+
+exports.size = ()=>{
+    return memory.length;
+}
+
+validateValue = (value)=>{
+    if(value<0||value>0xFF){
+        throw new Error('invalidValueException');
+    }
+    return true;
+}
+
+validatePointer = (pointer)=>{
+    if(pointer<0||pointer>=memory.length){
+        throw new Error('invalidPointerException');
+    }
+    return true;
+}
---
+++ tests/memory.test.js
@@ -0,0 +1,28 @@
+const mem = require('../src/memory');
+test('initialise memory',()=>{
+    mem.init();
+    expect(mem.size()).toBe(parseInt(process.env.MEMORY_SIZE)+1);
+    for(var i = 0x0000; i<mem.size; i++){
+        expect(mem.load(i)).toBe(0x0000);
+    }
+})
+test('set valid values', ()=>{
+    mem.init();
+    mem.set(0x0000, 0x00);
+    mem.set(0xF000, 0x0F);
+    mem.set(0xFFFF, 0xFF);
+    expect(mem.load(0x0000)).toBe(0x00);
+    expect(mem.load(0xF000)).toBe(0x0F);
+    expect(mem.load(0xFFFF)).toBe(0xFF);
+    mem.init();
+})
+
+
+test('set invalid values', ()=>{
+    mem.init();
+    expect(()=>{mem.set(0x10000, 0xFF)}).toThrowError('invalidPointerException');
+    expect(()=>{mem.set(-0x0001, 0xFF)}).toThrowError('invalidPointerException');
+    expect(()=>{mem.set(0x00FF, 0x100)}).toThrowError('invalidValueException');
+    expect(()=>{mem.set(0xFF00, -0x01)}).toThrowError('invalidValueException');
+    mem.init();
+})
