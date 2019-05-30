f07d3bbbbffc34d39cd70c0b58dcca4f3a9b5bc6
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-05-30 21:22:19 +0200

    restructured commands and removed commands.test.js

--- src/commands.js
+++ src/commands.js
@@ -1,26 +1,29 @@
+const mem = require('./memory');
 const cpu = require('./CPU');
-exports.LD_nn_n = (register, eightBitValue)=>{
-    switch(register){
-        case 'B':
-            cpu.setB(eightBitValue);
-            break;
-        case 'C':
-            cpu.setC(eightBitValue);
-            break;
-        case 'D':
-            cpu.setD(eightBitValue);
-            break;
-        case 'E':
-            cpu.setE(eightBitValue);
-            break;
-        case 'H':
-            cpu.setH(eightBitValue);
-            break;
-        case 'L':
-            cpu.setL(eightBitValue);
-            break;
-        default:
-            throw new Error('invalidRegisterExeption');
-    }
+
+LD_nn_n = (value)=>{
+    cpu.increaseCounter();
+    mem.set(mem.load(cpu.getProgramCounter()), value);
     return 8;
+}
+
+exports.opcode = (opcode)=>{
+    switch(opcode){
+        /*
+            8-Bit Loads
+            1. LD_nn_n
+        */
+        case 0x06:
+            return LD_nn_n(cpu.getB());
+        case 0x0E:
+            return LD_nn_n(cpu.getC());
+        case 0x16:
+            return LD_nn_n(cpu.getD());
+        case 0x1E:
+            return LD_nn_n(cpu.getE());
+        case 0x26:
+            return LD_nn_n(cpu.getH());
+        case 0x2E:
+            return LD_nn_n(cpu.getL());
+    }
 }
