2b78918107c85903b3b61748f94df56b4bf6eddb
Hans Ludvig Flått-Bjørnstad <hansludvig@hotmail.no>
2019-07-22 21:55:23 +0200

    added html output after test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
     module.exports = {
         "verbose":true,
         "collectCoverage": true,
-        "coverageReporters": ["text"],
+        "coverageReporters": ["text", "html"],
         "collectCoverageFrom":[
             "./src/*.js",
             "!./src/index.js",
